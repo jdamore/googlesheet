66fcf66a7d5203ba7d8a1231222de34b0a79b849
Jean D'Amore <jean.damore@gmail.com>
2021-04-28 23:47:51 +0200

    Finished Django setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-set -e
-conda create -n googlesheet python=3.6.3 anaconda
+#set -e
+conda create -y -n googlesheet python=3.6.3
+conda install -n googlesheet pip
+conda activate googlesheet
+python3 -m pip install --upgrade pip
+python3 -m pip install Django
 echo
