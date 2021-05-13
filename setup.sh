7f3f601eed0eea6339baa778ca4ba199ea3e240a
Jean D'Amore <jean.damore@gmail.com>
2021-05-13 15:06:23 +0200

    Added Django site

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
-#set -e
-conda create -y -n googlesheet python=3.6.3
-conda install -n googlesheet pip
-conda activate googlesheet
+set -e
 python3 -m pip install --upgrade pip
-python3 -m pip install Django
+python3 -m pip install django
 echo
