ce99eef2408907a1727fb31d40c02f2df414e080
Jean D'Amore <jean.damore@gmail.com>
2021-05-13 16:25:37 +0200

    Added code to read Google Spreadsheet

--- djangosite/googlesheet/views.py
+++ djangosite/googlesheet/views.py
@@ -1,4 +1,43 @@
+
 from django.http import HttpResponse
 
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2 import service_account
+
+SERVICE_ACCOUNT_FILE = os.getenv('GOOGLESHEET_SERVICE_ACCOUNT')
+
+SCOPES = [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/drive.readonly',
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/spreadsheets.readonly']
+        
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '1D1UxlZCocTj5UTnssNe0rSOH4BRPf014yZYxCCuNiqg'
+SPREADSHEET_RANGE = 'Data!A2:E'
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the Google Sheet index.")
+
+    # Authenticate
+    creds = None
+    if os.path.exists(SERVICE_ACCOUNT_FILE):
+        creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    else:
+        return HttpResponse('Cannot find file ' + SERVICE_ACCOUNT_FILE)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SPREADSHEET_RANGE).execute()
+    values = result.get('values', [])
+
+    if not values:
+        return HttpResponse('No values in Spreadsheet ' + SPREADSHEET_ID)
+    else:
+        return HttpResponse('Finished reading spreadsheet %s: first row is %s' % (SPREADSHEET_ID, values[0]))
--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 set -e
 python3 -m pip install --upgrade pip
 python3 -m pip install django
-echo
+#python3 -m pip install google_oauth2_tool
+python3 -m pip install google-api-python-client
+python3 -m pip install google-auth-httplib2
+python3 -m pip install google-auth-oauthlib
+export GOOGLESHEET_SERVICE_ACCOUNT="/Users/jdamore/dev/projects/googlesheet/googlesheet-service-account.json"
+
