d59219f0c93d64ce3ef58e92e008b872423bf197
Jean D'Amore <jean.damore@n26.com>
2021-05-15 10:37:51 +0200

    Added venv creation in setup script. Added README

---
+++ README.md
@@ -0,0 +1,30 @@
+# Google Sheet vissualisation app
+
+## Setup
+
+### Prerequisites
+
+You must have python3 installed on you machine.
+Setup has been developped and tested on MacOSX 10.15 with zsh.
+
+### Setup
+
+To setup the environment and project run:
+
+```
+cd {googlesheeet}
+. ./setup.sh
+```
+
+This will create a local virtual environment in the `.venv` directory and install all required libraries.
+
+### Start the server locally
+
+To start the dev server run:
+
+```
+cd {googlesheeet}
+./start.sh
+```
+
+Then you can hit the main URL at [http://127.0.0.1:8000/googlesheet](http://127.0.0.1:8000/googlesheet).
--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
-set -e
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+python3 -m venv $SCRIPTPATH/.venv
+chmod +x $SCRIPTPATH/.venv/bin/activate
+. $SCRIPTPATH/.venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install django
 python3 -m pip install google-api-python-client
 python3 -m pip install google-auth-httplib2
 python3 -m pip install google-auth-oauthlib
-export GOOGLESHEET_SERVICE_ACCOUNT="/Users/jdamore/dev/projects/googlesheet/googlesheet-service-account.json"
 
+export GOOGLESHEET_SERVICE_ACCOUNT="${SCRIPTPATH}/googlesheet-service-account.json"
+echo set GOOGLESHEET_SERVICE_ACCOUNT to $GOOGLESHEET_SERVICE_ACCOUNT
