9d8b31af35876992b659fe2433b604ee98dbb645
Jean D'Amore <jean.damore@n26.com>
2021-05-19 20:59:19 +0200

    Small changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
+# Setup Virtual Env
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
 python3 -m venv $SCRIPTPATH/.venv
 chmod +x $SCRIPTPATH/.venv/bin/activate
-. $SCRIPTPATH/.venv/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install django
-python3 -m pip install google-api-python-client
-python3 -m pip install google-auth-httplib2
-python3 -m pip install google-auth-oauthlib
+chmod +x $SCRIPTPATH/.venv/bin/pip3
+source $SCRIPTPATH/.venv/bin/activate
 
+# Install dependencies
+$SCRIPTPATH/.venv/bin/pip3 install --upgrade pip
+$SCRIPTPATH/.venv/bin/pip3 install django
+$SCRIPTPATH/.venv/bin/pip3 install google-api-python-client
+$SCRIPTPATH/.venv/bin/pip3 install google-auth-httplib2
+$SCRIPTPATH/.venv/bin/pip3 install google-auth-oauthlib
+
+# Set program environment variables
 export GOOGLESHEET_OAUTH2_CLIENT="${SCRIPTPATH}/googlesheet-oauth2-client.json"
 echo set GOOGLESHEET_OAUTH2_CLIENT to $GOOGLESHEET_OAUTH2_CLIENT
